a1cd3121e8c86c3c460974fcf4db3fde71b0a8b9
SaiKumarNaidu-PeddiNeni <68958425+SaiKumarNaidu-PeddiNeni@users.noreply.github.com>
2021-02-15 12:22:04 +0530

    Add files via upload

---
+++ SpringBootSonarCube/src/main/java/com/sonarcube/SpringBootSonarCubeApplication.java
@@ -0,0 +1,13 @@
+package com.sonarcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarCubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSonarCubeApplication.class, args);
+	}
+
+}
